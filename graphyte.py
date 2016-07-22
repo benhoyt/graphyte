277fd70d5c403981c017ff0ac905b54123134ad6
Ben Hoyt <benhoyt@gmail.com>
2016-07-22 13:38:30 -0400

    Add graphyte source and tests

---
+++ graphyte.py
@@ -0,0 +1,217 @@
+"""Send data to Graphite metrics server (synchronously or on a background thread).
+
+Example synchronous usage:
+--------------------------
+
+# Sends b'system.sync.foo.bar 42 {timestamp}\n' to graphite.example.com:2003
+import graphyte
+graphyte.init('graphite.example.com', prefix='system.sync')
+graphyte.send('foo.bar', 42)
+
+
+Example asynchronous usage:
+---------------------------
+
+# Sends b'system.async.foo.bar 42 {timestamp}\n' to graphite.example.com:2003
+# on background thread, sending every 10 seconds
+graphyte.init('graphite.example.com', prefix='system.async', interval=10)
+graphyte.send('foo.bar', 42)
+
+
+Example asynchronous usage with multiple senders:
+-------------------------------------------------
+
+# Sends b'system.one.foo.bar1 42 {timestamp}\n' to graphite1.example.com:2003 and
+# b'system.two.foo.bar2 43 {timestamp}\n' to graphite2.example.com:2003
+sender1 = graphyte.Sender('graphite1.example.com', prefix='system.one')
+sender2 = graphyte.Sender('graphite2.example.com', prefix='system.two', interval=10)
+sender1.send('foo.bar1', 42)
+sender2.send('foo.bar2', 43)
+
+"""
+
+import atexit
+import logging
+try:
+    import queue
+except ImportError:
+    import Queue as queue  # Python 2.x compatibility
+import socket
+import threading
+import time
+
+__all__ = ['Sender', 'init', 'send']
+
+default_sender = None
+logger = logging.getLogger(__name__)
+
+
+class Sender:
+    def __init__(self, host, port=2003, prefix=None, timeout=5, interval=None,
+                 queue_size=None, log_sends=False):
+        """Initialize a Sender instance, starting the background thread to
+        send messages at given interval (in seconds) if "interval" is not
+        None.
+        """
+        self.host = host
+        self.port = port
+        self.prefix = prefix
+        self.timeout = timeout
+        self.interval = interval
+        self.log_sends = log_sends
+
+        if self.interval is not None:
+            if queue_size is None:
+                queue_size = int(round(interval)) * 100
+            self._queue = queue.Queue(maxsize=queue_size)
+            self._thread = threading.Thread(target=self._thread_loop)
+            self._thread.daemon = True
+            self._thread.start()
+            atexit.register(self.stop)
+
+    def __del__(self):
+        self.stop()
+
+    def stop(self):
+        """Tell the sender thread to finish and wait for it to stop sending
+        (should be at most "timeout" seconds).
+        """
+        if self.interval is not None:
+            self._queue.put_nowait(None)
+            self._thread.join()
+            self.interval = None
+
+    def build_message(self, metric, value, timestamp):
+        """Build a Graphite message to send and return it as a byte string."""
+        if not metric or metric.split(None, 1)[0] != metric:
+            raise ValueError('"metric" must not have whitespace in it')
+        if not isinstance(value, (int, float)):
+            raise TypeError('"value" must be an int or a float, not a {}'.format(
+                    type(value).__name__))
+
+        message = u'{}{} {} {}\n'.format(
+            self.prefix + '.' if self.prefix else '',
+            metric,
+            value,
+            int(round(timestamp)),
+        )
+        message = message.encode('utf-8')
+        return message
+
+    def send(self, metric, value, timestamp=None):
+        """Send given metric and (int or float) value to Graphite host.
+        Performs send on background thread if "interval" was specified when
+        creating this Sender.
+        """
+        if timestamp is None:
+            timestamp = time.time()
+        message = self.build_message(metric, value, timestamp)
+
+        if self.interval is None:
+            self.send_socket(message)
+        else:
+            try:
+                self._queue.put_nowait(message)
+            except queue.Full:
+                logger.error('queue full when sending {!r}'.format(message))
+
+    def send_socket(self, message):
+        """Low-level function to send message bytes to this Sender's socket.
+        You should usually call send() instead of this function (unless you're
+        subclassing or writing unit tests).
+        """
+        if self.log_sends:
+            start_time = time.time()
+        try:
+            conn = socket.create_connection((self.host, self.port), self.timeout)
+            conn.send(message)
+            conn.close()
+        except Exception as error:
+            logger.error('error sending message {!r}: {}'.format(message, error))
+        else:
+            if self.log_sends:
+                elapsed_time = time.time() - start_time
+                logger.info('sent message {!r} to {}:{} in {:.03f} seconds'.format(
+                        message, self.host, self.port, elapsed_time))
+
+    def _thread_loop(self):
+        """Background thread used when Sender is in asynchronous/interval mode."""
+        last_check_time = time.time()
+        messages = []
+        while True:
+            # Get first message from queue, blocking until the next time we
+            # should be sending
+            time_since_last_check = time.time() - last_check_time
+            time_till_next_check = max(0, self.interval - time_since_last_check)
+            try:
+                message = self._queue.get(timeout=time_till_next_check)
+            except queue.Empty:
+                pass
+            else:
+                if message is None:
+                    # None is the signal to stop this background thread
+                    break
+                messages.append(message)
+
+                # Get any other messages currently on queue without blocking,
+                # paying attention to None ("stop thread" signal)
+                should_stop = False
+                while True:
+                    try:
+                        message = self._queue.get_nowait()
+                    except queue.Empty:
+                        break
+                    if message is None:
+                        should_stop = True
+                        break
+                    messages.append(message)
+                if should_stop:
+                    break
+
+            # If it's time to send, send what we've collected
+            current_time = time.time()
+            if current_time - last_check_time >= self.interval:
+                last_check_time = current_time
+                if messages:
+                    self.send_socket(b''.join(messages))
+                    messages = []
+
+        # Send any final messages before exiting thread
+        if messages:
+            self.send_socket(b''.join(messages))
+
+
+def init(*args, **kwargs):
+    """Initialize default Sender instance with given args."""
+    global default_sender
+    default_sender = Sender(*args, **kwargs)
+
+
+def send(*args, **kwargs):
+    """Send message using default Sender instance."""
+    default_sender.send(*args, **kwargs)
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('metric',
+                        help='name of metric to send')
+    parser.add_argument('value', type=float,
+                        help='numeric value to send')
+    parser.add_argument('-s', '--server', default='localhost',
+                        help='hostname of Graphite server to send to, default %(default)s')
+    parser.add_argument('-p', '--port', type=int, default=2003,
+                        help='port to send message to, default %(default)d')
+    parser.add_argument('-t', '--timestamp', type=int,
+                        help='Unix timestamp for message (defaults to current time)')
+    parser.add_argument('-q', '--quiet', action='store_true',
+                        help="quiet mode (don't log send to stdout)")
+    args = parser.parse_args()
+
+    if not args.quiet:
+        logging.basicConfig(level=logging.INFO, format='%(message)s')
+
+    sender = Sender(args.server, port=args.port, log_sends=not args.quiet)
+    sender.send(args.metric, args.value, timestamp=args.timestamp)
---
+++ test_graphyte.py
@@ -0,0 +1,139 @@
+"""Unit tests for the graphyte module."""
+
+import re
+try:
+    import socketserver
+except ImportError:
+    import SocketServer as socketserver  # Python 2.x compatibility
+import time
+import unittest
+
+import graphyte
+
+
+class TestSender(graphyte.Sender):
+    def __init__(self, *args, **kwargs):
+        graphyte.Sender.__init__(self, 'dummy_host', *args, **kwargs)
+        self.messages = []
+
+    def send_socket(self, message):
+        self.messages.append(message)
+
+    def pop_message(self):
+        assert self.messages, 'no messages sent'
+        return self.messages.pop(0)
+
+
+class TestHandler(socketserver.BaseRequestHandler):
+    messages = []
+
+    def handle(self):
+        message = self.request.recv(1024)
+        self.messages.append(message)
+
+    @classmethod
+    def pop_message(cls):
+        assert cls.messages, 'no messages sent'
+        return cls.messages.pop(0)
+
+
+class TestBuildMessage(unittest.TestCase):
+    def test_no_prefix(self):
+        sender = TestSender()
+        self.assertEqual(sender.build_message('foo.bar', 42, 12345),
+                         b'foo.bar 42 12345\n')
+        self.assertEqual(sender.build_message('boo.far', 42.1, 12345.6),
+                         b'boo.far 42.1 12346\n')
+
+    def test_unicode(self):
+        sender = TestSender()
+        self.assertEqual(sender.build_message(u'\u201cfoo.bar\u201d', 42, 12345),
+                         b'\xe2\x80\x9cfoo.bar\xe2\x80\x9d 42 12345\n')
+
+    def test_prefix(self):
+        sender = TestSender(prefix='pr.efix')
+        self.assertEqual(sender.build_message('boo.far', 567, 12347),
+                         b'pr.efix.boo.far 567 12347\n')
+
+    def test_exceptions(self):
+        sender = TestSender()
+        with self.assertRaises(TypeError):
+            sender.build_message('foo.bar', 'x', 12346)
+        with self.assertRaises(ValueError):
+            sender.build_message('foo bar', 42, 12346)
+
+
+class TestSynchronous(unittest.TestCase):
+    def test_timestamp_specified(self):
+        sender = TestSender()
+        sender.send('foo', 42, timestamp=12345)
+        self.assertEqual(sender.pop_message(), b'foo 42 12345\n')
+
+    def test_timestamp_generated(self):
+        sender = TestSender()
+        send_time = time.time()
+        sender.send('foo', 42)
+        match = re.match(b'^foo 42 (\\d+)\\n$', sender.pop_message())
+        self.assertIsNotNone(match)
+        timestamp = int(match.group(1))
+        self.assertTrue(send_time - 2 <= timestamp <= send_time + 2)
+
+
+class TestSendSocket(unittest.TestCase):
+    def setUp(self):
+        self.server = socketserver.TCPServer(('127.0.0.1', 2003), TestHandler)
+        self.server.timeout = 1.0
+
+    def tearDown(self):
+        self.server.server_close()
+
+    def test_send_socket(self):
+        graphyte.init('127.0.0.1')
+        graphyte.send('foo', 42, timestamp=12345)
+        graphyte.send('bar', 43.5, timestamp=12346)
+        self.server.handle_request()
+        self.server.handle_request()
+        self.assertEqual(TestHandler.pop_message(), b'foo 42 12345\n')
+        self.assertEqual(TestHandler.pop_message(), b'bar 43.5 12346\n')
+
+
+class TestInterval(unittest.TestCase):
+    def setUp(self):
+        self.sender = TestSender(interval=0.1)
+
+    def tearDown(self):
+        self.sender.stop()
+
+    def test_stop_after_message(self):
+        self.sender.send('foo', 42, timestamp=12345)
+        self.sender.stop()
+        self.assertEqual(self.sender.pop_message(), b'foo 42 12345\n')
+
+    def test_stop_immediately(self):
+        self.sender.stop()
+        self.assertEqual(self.sender.messages, [])
+
+    def test_send_none(self):
+        time.sleep(0.2)
+        self.assertEqual(self.sender.messages, [])
+
+    def test_send_one(self):
+        self.sender.send('foo', 42, timestamp=12345)
+        time.sleep(0.2)
+        self.assertEqual(self.sender.pop_message(), b'foo 42 12345\n')
+
+    def test_send_multiple(self):
+        self.sender.send('foo', 42, timestamp=12345)
+        self.sender.send('bar', 43, timestamp=12346)
+        self.sender.send('baz', 44, timestamp=12347)
+        time.sleep(0.2)
+        self.assertEqual(self.sender.pop_message(),
+                         b'foo 42 12345\nbar 43 12346\nbaz 44 12347\n')
+
+        self.sender.send('buz', 45, timestamp=12348)
+        time.sleep(0.2)
+        self.assertEqual(self.sender.pop_message(), b'buz 45 12348\n')
+
+
+if __name__ == '__main__':
+    unittest.main()
